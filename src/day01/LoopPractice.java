0d72f0acd7de6ca3d029c62dd6baab395542cfff
tolkunasanaly <tolkusha@gmail.com>
2020-03-04 06:49:35 -0500

    update

--- src/day01/LoopPractice.java
+++ src/day01/LoopPractice.java
@@ -4,6 +4,7 @@ public class LoopPractice {
     public static void main(String[] args) {
         for (int i=0; i<10; i+=2){
             System.out.println("Counter = " + i);
+            System.out.println("Changes made later");
         }
     }
 }
